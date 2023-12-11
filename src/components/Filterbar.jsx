7108fb2bf66b692e0c50b074c7b8242c33159e78
vikasipar <vickyipar01@gmail.com>
2023-12-11 22:51:00 +0530

    implement category-based products filtering

---
+++ src/components/Filterbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Filterbar(props) {
+
+    const categories = ['All Products','jewelery','electronics',`men's clothing`,`women's clothing`];
+    
+  return (
+    <div className="categories w-[60%] mx-auto my-5 flex justify-around">
+        {categories.map((category) => (
+            <span key={category} 
+            onClick={() => props.handleClick(category)}
+            className={`border-2 cursor-pointer px-5 border-gray-900 ${props.select===category ? ' bg-gray-900 text-white' : 'border-gray-800 text-gray-900'}`}>{category[0].toUpperCase()+category.slice(1)}</span>
+        ))}
+      </div>
+  )
+}
+
+export default Filterbar;
